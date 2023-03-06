a3ecea926130ee1dfa63649ef30877e531f8b616
Sonali Raha <118559924+sonaliraha25@users.noreply.github.com>
2023-03-06 22:50:03 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(function(){
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
+    });
+});
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+  
+    for (var i = 0; i < reveals.length; i++) {
+      var windowHeight = window.innerHeight;
+      var elementTop = reveals[i].getBoundingClientRect().top;
+      var elementVisible = 150;
+  
+      if (elementTop < windowHeight - elementVisible) {
+        reveals[i].classList.add("active");
+      } else {
+        reveals[i].classList.remove("active");
+      }
+    }
+  }
+  
+  window.addEventListener("scroll", reveal);
+  
